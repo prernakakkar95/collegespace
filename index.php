ba25d37fc7db011c1b58d896874bd7b8b7d0906c
prerna kakkar <pkakkar95@gmail.com>
2015-10-26 11:50:20 +0530

    first commit:setting of index.php and addition of css files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>COLLEGESPACE||TASK1</title>
+
+        <!-- BOOTSTRAP CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+        <!-- FONT ICONS -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/icomoon.css">
+</head>
+<body>
+ <header>
+<!--  written nsit with collegespace -->
+<!-- nav-->
+</header>
+<!-- carousel -images of college-->
+<!-- in page/new tab sections-->
+
+</body>
+</html>
