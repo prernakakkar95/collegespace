68eb12fc62820e98750a8640bf97a8c41f40e160
yjyotiyadav <jyotiy47@gmail.com>
2015-10-29 02:01:39 +0530

    simple dropdown menu for the page.

--- index.php
+++ index.php
@@ -10,11 +10,39 @@
         <link rel="stylesheet" href="assets/css/font-awesome.min.css">
         <link rel="stylesheet" href="assets/css/icomoon.css">
 </head>
-<body>
- <header>
+<body><header>
 <!--  written nsit with collegespace -->
 <!-- nav-->
 </header>
+
+<div class="a">
+<img src="cs.jpg" height=188px width="100%"/>
+</div>
+<div class="b">
+<abbr vaue="admin"> <input type="button" id="btn1" value="login faculty "/></div></abbr>
+<div class="c">
+<input type="button" id="btn1" value="login student"/>
+</div>
+<div class="d">
+<input type="textbox" id="search" placeholder="SEARCH" width=50px />
+<input type="button" id="btn1" value="go"/>
+</div>
+<div class="dropdown">
+
+    <ul>
+        <li><a href="#">HOME &#9662;</a>
+            <ul>
+             <li><a href="#">ABOUT NSIT</a></li>   
+             <li><a href="#">ABOUT CS</a></li>   
+             <li><a href="#">CAMPUS TOUR</a></li>   
+                     
+            </ul>
+         <li><a href="#">OUR FACULTY</a></li>          
+         <li><a href="#">OTHER RESOURCES</a></li>
+         <li><a href="#">THE CS TEAM</a></li>
+         <li><a href="#">CONTACT NSIT </a></li>
+    </ul></div>
+ 
 <!-- carousel -images of college-->
 <!-- in page/new tab sections-->
 <!--js-->
