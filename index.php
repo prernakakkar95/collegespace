ada910f75e4901f8e0a005a3b354d50f22f7aeec
JeeveshN <narang.jeevesh@gmail.com>
2015-10-28 22:01:11 +0530

    nav bar and carousel
    added random images in carousel will change when we get the images to be
    used

--- index.php
+++ index.php
@@ -1,26 +1,112 @@
 <!DOCTYPE html>
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
 <title>COLLEGESPACE||TASK1</title>
 
-        <!-- BOOTSTRAP CSS -->
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!-- BOOTSTRAP CSS -->
+	<link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="./assets/css/main.css">
 
-        <!-- FONT ICONS -->
-        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-        <link rel="stylesheet" href="assets/css/icomoon.css">
+	<!-- FONT ICONS -->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/icomoon.css">
+
+	
 </head>
-<body><header>
-<!--  written nsit with collegespace -->
-<!-- nav-->
-</header>
+<body>
+	<!--  written nsit with collegespace -->
+	<div class="CS-cover">
+		<div class="container">
+		<!--Add image in main.css-->	
+		</div>
+	</div>
+	<!--nav-->
+	<nav class="navbar navbar-inverse">
+   		<div class="container-fluid">
+   			 <div class="navbar-header">
+      			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        			<span class="icon-bar"></span>
+        			<span class="icon-bar"></span>
+        		<span class="icon-bar"></span>                        
+      			</button>
+      			<a class="navbar-brand" href="#">College Space</a>
+    		</div>
+    		<div class="collapse navbar-collapse" id="myNavbar">
+      			<ul class="nav navbar-nav ">
+        			<li class="active"><a href="#">HOME</a></li>
+					<li><a href="#">OUR FACULTY</a></li>
+					<li><a href="#">STUDENT RESOURCES</a></li>
+					<li><a href="#">OTHER RESOURCES</a></li>
+					<li><a href="#">CONTACT NSIT</a></li>
+					<li><a href="#">THE CS TEAM</a></li>
+        		</ul>   
+    		</div>
+  		</div>
+	</nav>
 
+	<!-- carousel -images of college-->          <!-- Replace the images and Captions Later-->
+	<div class="Roller">
+		<div class="container">
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    	<ol class="carousel-indicators">
+      		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      		<li data-target="#myCarousel" data-slide-to="1"></li>
+      		<li data-target="#myCarousel" data-slide-to="2"></li>
+      		<li data-target="#myCarousel" data-slide-to="3"></li>
+    	</ol>
 
- 
-<!-- carousel -images of college-->
-<!-- in page/new tab sections-->
-<!--js-->
-  <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/smoothscroll.js"></script>
+    <!-- Wrapper for slides -->
+    	<div class="carousel-inner" role="listbox">
+      		<div class="item active">
+        		<img src="./assets/images/tomorrowland-2012.jpg"  style="width:100%;height:100%">
+         		<div class="carousel-caption">
+         			<h3>2012</h3>
+         		</div>
+      		</div>
+
+      		<div class="item">
+        		<img src="./assets/images/2013.jpg" style="width:100%;height:100%">
+         		<div class="carousel-caption">
+         			<h3>2013</h3>
+         		</div>
+      		</div>
+    
+      		<div class="item">
+        			<img src="./assets/images/2014.jpg" style="width:100%;height:100%">
+ 		  			<div class="carousel-caption">
+         				<h3>2014</h3>
+         			</div>    
+      		</div>
+
+      		<div class="item">
+        		<img src="./assets/images/2015.jpg"  style="width:100%;height:100%">
+         		<div class="carousel-caption">
+         			<h3>2015</h3>
+         		</div>
+      		</div>
+    	</div>
+
+    <!-- Left and right controls -->
+    	<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"></a>
+    	<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"></a>
+  	</div>
+		</div>
+	</div>
+	<!-- in page/new tab sections-->
+	
+	<!--Footer-->
+	<div class="Footer">
+		<div class="container">
+			<!--<img class="pull-left" src="logocs.jpg" style=" height:50px" />-->
+			<p>Powered by College Space</p>
+		</div>
+	</div>
+
+	<!--js-->
+	<script src="./assets/js/jquery.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <script src="./assets/js/smoothscroll.js"></script>
 </body>
 </html>
